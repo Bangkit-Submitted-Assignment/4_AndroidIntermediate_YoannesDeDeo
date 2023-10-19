75bd1763821adc0c62eabb54c92ef8c66f15a242
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-10-20 01:09:20 +0700

    Create RegisterResponse.java

---
+++ StoryAppDicoding/app/src/main/java/com/dicoding/storyappdicoding/api/RegisterResponse.java
@@ -0,0 +1,22 @@
+package com.dicoding.storyappdicoding.api;
+
+public class RegisterResponse{
+	private boolean error;
+	private String message;
+
+	public void setError(boolean error){
+		this.error = error;
+	}
+
+	public boolean isError(){
+		return error;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+}
